4d9e688ee46f647c37d25dda52f6c28d3f476b57
EmreUstaoglu <55128324+EmreUstaoglu@users.noreply.github.com>
2019-11-13 09:41:28 +0100

    Add files via upload

---
+++ opdracht 1 emre.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace opdracht_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("Welkom Bij de BMI berekening");
+            Console.WriteLine("Geprogameerd door Emre Ustaoglu");
+
+             Console.Write("Gewicht in kilogram: ");
+            int kg;
+            kg = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Hoogte in centimers: ");
+            int m;
+            m = Convert.ToInt32(Console.ReadLine());
+            
+        
+           double BMI = kg / ( ( m / 100.0 ) * ( m / 100.0 ) );
+            Console.WriteLine("Je BMI is '{0}'", BMI);
+        }
+    }
+}
